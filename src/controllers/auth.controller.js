467424ec92b3ed4aad6a40d8443b4ff3dc0af8b6
MiguelJimenez99 <migueljimenez171999@gmail.com>
2025-05-07 15:06:14 -0500

    se modifico codigo de error en el controlador login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,14 +47,14 @@ exports.login = async (req, res) => {
     const user = await User.findOne({ email });
 
     if (!user) {
-      return res.status(400).json({
+      return res.status(404).json({
         message: "Usuario no encontrado",
       });
     }
 
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      return res.status(400).json({
+      return res.status(401).json({
         message: "Contraseña incorrecta",
       });
     }
